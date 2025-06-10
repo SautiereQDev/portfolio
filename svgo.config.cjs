e04ac050c7fe369db55a14599b91120b02dcd05e
Quentin Sautiere <contact@quentinsautiere.com>
2025-06-10 15:13:42 +0200

    fix: cjs module error
    Add eslint ignore to the first line

--- svgo.config.cjs
+++ svgo.config.cjs
@@ -1,39 +1,40 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    plugins: [
-        {
-            name: 'preset-default',
-            params: {
-                overrides: {
-                    // Préserver les éléments visuels importants
-                    removeViewBox: false,
-                    cleanupIds: false,
-                    removeTitle: false,
-                    removeDesc: false,
+  plugins: [
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          // Préserver les éléments visuels importants
+          removeViewBox: false,
+          cleanupIds: false,
+          removeTitle: false,
+          removeDesc: false,
 
-                    // PRÉSERVER LES COULEURS ET STYLES
-                    convertColors: false,           // Ne pas convertir les couleurs
-                    convertShapeToPath: false,      // Garder les formes originales
-                    mergeStyles: false,             // Ne pas fusionner les styles
+          // PRÉSERVER LES COULEURS ET STYLES
+          convertColors: false,           // Ne pas convertir les couleurs
+          convertShapeToPath: false,      // Garder les formes originales
+          mergeStyles: false,             // Ne pas fusionner les styles
 
-                    // Optimisations sûres seulement
-                    removeUselessDefs: true,
-                    removeEmptyAttrs: true,
-                    removeEmptyText: true,
-                    removeEmptyContainers: true,
-                    collapseGroups: false,          // Ne pas réorganiser les groupes
-                },
-            },
+          // Optimisations sûres seulement
+          removeUselessDefs: true,
+          removeEmptyAttrs: true,
+          removeEmptyText: true,
+          removeEmptyContainers: true,
+          collapseGroups: false,          // Ne pas réorganiser les groupes
         },
+      },
+    },
 
-        // Supprimer uniquement les dimensions fixes pour la responsivité
-        'removeDimensions',
+    // Supprimer uniquement les dimensions fixes pour la responsivité
+    'removeDimensions',
 
-        // Optimisations très conservatrices
-        {
-            name: 'cleanupNumericValues',
-            params: {
-                floatPrecision: 3,  // Plus de précision pour préserver les détails
-            },
-        },
-    ],
+    // Optimisations très conservatrices
+    {
+      name: 'cleanupNumericValues',
+      params: {
+        floatPrecision: 3,  // Plus de précision pour préserver les détails
+      },
+    },
+  ],
 };
